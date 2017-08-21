3d07814a582e71a16cfe376bbc476089b8006a42
solaiswaminathan <30690897+solaiswaminathan@users.noreply.github.com>
2017-08-21 17:21:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,26 +30,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '/madi.png'));
 });
-var app = express();
-app.use(morgan('combined'));
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-app.get('/article-one', function (req, res) {
-    res.send('Article one requested and will be served');
-});
-app.get('/article-two', function (req, res) {
-    res.send('Article two requested and will be served');
-});
-app.get('/article-three', function (req, res) {
-    res.send('Article three requested and will be served');
-});
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
